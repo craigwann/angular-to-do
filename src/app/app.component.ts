0b4f9d39e4de47cea29e31d5c07af11545a4b2e0
craig wann and scott fraser <craig@>
2018-08-13 10:40:32 -0700

    finish to-do monday homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear()
-  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
-
+  tasks: Task[] = [
+      new Task('Finish weekend Angular homework for Epicodus course'),
+      new Task('Begin brainstorming possible JavaScript group projects'),
+      new Task('Add README file to last few Angular repos on GitHub')
+    ];
 }
